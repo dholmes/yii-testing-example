a3a4b0d0068a1abd57b0024230697a3b8cd6d065
Dan Holmes <daniel.holmes@gmail.com>
2013-11-02 20:17:44 -0500

    connecting to local database

--- protected/config/main.php
+++ protected/config/main.php
@@ -48,15 +48,13 @@ return array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
 		// uncomment the following to use a MySQL database
-		/*
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=example_db',
 			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
+			'username' => 'example_user',
+			'password' => '1234',
 			'charset' => 'utf8',
 		),
-		*/
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
