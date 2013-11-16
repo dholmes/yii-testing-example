a8da48d7191269c80a020affbb9e6af6754f3b6f
Dan Holmes <daniel.holmes@gmail.com>
2013-11-16 16:35:17 -0600

    Hooking up libraries for phpunit testing

--- protected/tests/bootstrap.php
+++ protected/tests/bootstrap.php
@@ -1,10 +1,28 @@
 <?php
 
+require_once(dirname(__FILE__).'/../../vendor/autoload.php');
+
 // change the following paths if necessary
-$yiit=dirname(__FILE__).'/../../../vendor/yiisoft/yii/framework/yiit.php';
+$yiit=dirname(__FILE__).'/../../vendor/yiisoft/yii/framework/yiit.php';
 $config=dirname(__FILE__).'/../config/test.php';
 
-require_once($yiit);
-require_once(dirname(__FILE__).'/WebTestCase.php');
+$kernel = AspectMock\Kernel::getInstance();
+$kernel->init([
+    'debug' => true,
+    'includePaths' => [
+        __DIR__ . '/../',
+        __DIR__ . '/../../vendor/yiisoft/yii/framework'
+    ],
+    'appDir' => __DIR__ . '/../',
+    'excludePaths' => [
+        __DIR__ . '/../config',
+        __DIR__ . '/../runtime',
+        __DIR__ . '/../../../vendor/yiisoft/yii/framework/base/CComponent.php',
+    ],
+]);
+$kernel->loadFile($yiit);
+
+$kernel->loadFile(dirname(__FILE__).'/WebTestCase.php');
+
 
 Yii::createWebApplication($config);
